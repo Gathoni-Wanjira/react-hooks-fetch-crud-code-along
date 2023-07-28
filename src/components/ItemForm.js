e694f8c6976276a01cba8dd5f4a2f8d558cc8d2d
Gathoni Wanjira <janetgathoni910@gmail.com>
2023-07-28 21:36:42 +0300

    Assignment Done

--- src/components/ItemForm.js
+++ src/components/ItemForm.js
@@ -1,37 +1,76 @@
+import { response } from "msw";
 import React, { useState } from "react";
 
-function ItemForm() {
-  const [name, setName] = useState("");
-  const [category, setCategory] = useState("Produce");
+function ItemForm({ handleUpdate }) {
+    const [name, setName] = useState("");
+    const [category, setCategory] = useState("Dairy");
 
-  return (
-    <form className="NewItem">
-      <label>
-        Name:
-        <input
-          type="text"
-          name="name"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        />
-      </label>
 
-      <label>
-        Category:
-        <select
-          name="category"
-          value={category}
-          onChange={(e) => setCategory(e.target.value)}
-        >
-          <option value="Produce">Produce</option>
-          <option value="Dairy">Dairy</option>
-          <option value="Dessert">Dessert</option>
-        </select>
-      </label>
 
-      <button type="submit">Add to List</button>
-    </form>
-  );
+
+    function postNewItem() {
+
+        const newPost = {
+            name: name,
+            category: category,
+            isInCart: false
+        }
+
+
+
+        fetch("http://localhost:4000/items", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body:
+                JSON.stringify(newPost)
+
+
+        })
+            .then(res => res.json())
+            .then(postedItem => {
+                console.log(postedItem)
+                handleUpdate(newPost)
+
+            })
+    }
+    function handlePost(e) {
+        e.preventDefault();
+        postNewItem();
+
+    }
+
+
+    return (
+
+        < form className="NewItem" onSubmit={handlePost} >
+            <label>
+                Name:
+                <input
+                    type="text"
+                    name="name"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                />
+            </label>
+
+            <label>
+                Category:
+                <select
+                    name="category"
+                    value={category}
+                    onChange={(e) => setCategory(e.target.value)}
+                >
+                    <option value="Produce">Produce</option>
+                    <option value="Dairy">Dairy</option>
+                    <option value="Dessert">Dessert</option>
+                </select>
+            </label>
+
+            <button type="submit">Add to List</button>
+        </form >
+    );
 }
 
 export default ItemForm;
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,36 +1,75 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import ItemForm from "./ItemForm";
 import Filter from "./Filter";
 import Item from "./Item";
 
 function ShoppingList() {
-  const [selectedCategory, setSelectedCategory] = useState("All");
-  const [items, setItems] = useState([]);
+    const [selectedCategory, setSelectedCategory] = useState("All");
+    const [items, setItems] = useState([]);
+    
 
-  function handleCategoryChange(category) {
-    setSelectedCategory(category);
-  }
+    function handleCategoryChange(category) {
+        setSelectedCategory(category);
+    }
 
-  const itemsToDisplay = items.filter((item) => {
-    if (selectedCategory === "All") return true;
+    const itemsToDisplay = items.filter((item) => {
+        if (selectedCategory === "All") return true;
 
-    return item.category === selectedCategory;
-  });
+        return item.category === selectedCategory;
+    });
 
-  return (
-    <div className="ShoppingList">
-      <ItemForm />
-      <Filter
-        category={selectedCategory}
-        onCategoryChange={handleCategoryChange}
-      />
-      <ul className="Items">
-        {itemsToDisplay.map((item) => (
-          <Item key={item.id} item={item} />
-        ))}
-      </ul>
-    </div>
-  );
+    function handleUpdateItem (updatedItem) {
+        console.log(updatedItem);
+       
+            const updatedItems = items.map((item) => {
+              if (item.id === updatedItem.id) {
+                return updatedItem;
+              } else {
+                return item;
+              }
+            });
+            setItems(updatedItems);
+
+    
+    }
+    
+
+
+    function fetchfunction() {
+        console.log("jhgf")
+        fetch("http://localhost:4000/items")
+            .then(res => res.json())
+            .then(items => {
+                console.log(items)
+               setItems(items);
+
+            })
+    }
+
+    useEffect(() => {
+        fetchfunction();
+        
+    },[])
+
+function handleUpdate (newItem){
+
+    setItems([...items, newItem])
+}
+
+    return (
+        <div className="ShoppingList">
+            <ItemForm handleUpdate = {handleUpdate}/>
+            <Filter
+                category={selectedCategory}
+                onCategoryChange={handleCategoryChange}
+            />
+            <ul className="Items">
+                {itemsToDisplay.map((item) => (
+                    <Item handleUpdateItem = {handleUpdateItem} key={item.id} item={item} />
+                ))}
+            </ul>
+        </div>
+    );
 }
 
 export default ShoppingList;
